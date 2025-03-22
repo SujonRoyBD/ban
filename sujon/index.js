24065e26b7f3e8c080ac32faf46eeedd912412f0
SujonRoyBD <sujonroy537527@gmail.com>
2025-03-22 20:20:52 +0600

    rex

---
+++ sujon/index.js
@@ -0,0 +1,158 @@
+// var friendsName =("sujon roy");
+// console.log(friendsName)
+
+// var roy = ["amar","sonar","bangla"];
+// console.log(roy[2]);
+
+// var Name ={l
+//     brand:"Hp",
+//     color:"smart",
+//     ssd:"256",
+//     Ram:"8 Gb",
+// };
+// console.log(Name.Ram);
+
+//hoistin an use:
+
+
+console.log(x === undefined);
+var x = 323;
+(function(){
+    console.log(x);
+    var x="local value";
+})();
+
+
+
+console.log(x===undefined);
+var x=5;
+(function(){
+    console.log(x);
+    var x="local value"
+})();
+
+
+var x= 12;
+switch (x){
+    case 10:
+    console.log("hello world");
+    break;
+    case 12:
+        console.log("not fine");
+        break;
+        default:
+            console.log("kono khuje pelam na");
+        
+
+}
+
+var sum =2;
+if(sum>=80){
+    console.log("A+");
+}
+else if(sum>=70){
+    console.log("A");
+}
+else if(sum>=60){
+    console.log("A-");
+}
+else if(sum>=50){
+    console.log("B");
+}
+else if(sum>=40){
+    console.log("C");
+}
+else{
+    console.log("Fail");
+}
+
+
+console.log(undefined==null);
+var x=5;
+(function(){
+    console.log(x);
+    var x="local value"
+})();
+
+
+console.log(x==null);
+var x =3;
+x=9;
+(function(){
+    console.log(x);
+    console.log(x);
+
+})();
+
+
+var sum = {
+    color:"red",
+    proprty:"green",
+    brand:"hp"
+}
+console.log(sum.color);
+
+var ren=["red","green","yello","blue"];
+var result=ren.splice[2];
+console.log(result);
+
+var x=50;
+switch(x){
+    case 12:
+    console.log("roy");
+    break;
+    case 33:
+        console.log("helo world");
+        break;
+        case 66:
+            console.log("i am fine");
+            break;
+            case 50:
+                console.log("i love you");
+                break;
+    }
+
+    var a = 5;
+    var b = 45;
+    var c = 25;
+    if  (a>b && a>c){
+       
+        console.log("a is big");
+
+    }
+    else if(b>a && b>c){
+        console.log("b is big");
+
+    }
+        else{
+            console.log("c is big");
+        }
+
+        // var a=34;
+        // document.write(prompt(a));
+        
+        
+
+        for(i=0;i>10;i++);
+        console.log(i);
+
+        var s=13;
+        var e=4;
+        
+        console.log(s/e);
+
+
+
+        
+        console.log(x === undefined);
+        var x = 323;
+        (function(){
+            console.log(x);
+            var x="local value";
+        })();
+        
+        link.style.color="green"
+        var link = document.getElementsByTagName("a")[0];
+
+
+
